01f4e8f52ed75c317bf9907ffd650066cb5a4e81
Jorge Carranza <ssgsj.carranza@gmail.com>
2022-01-03 20:38:09 -0600

    added login logo and login button, added styling to logo

--- components/Login.js
+++ components/Login.js
@@ -4,9 +4,15 @@ function Login() {
     return (
         <div className="bg-black relative text-white">
             <h1>Login</h1>
-            <div>
+            <div className="flex flex-col absolute z-50 h-4/6 items-center justify-center w-full">
                 {/* logo */}
+                <Image 
+                    src='https://links.papareact.com/3pi'
+                    height={200}
+                    width={200}
+                />
                 {/* login button */}
+                <button>Login into Metaverse</button>
             </div>
 
             <div className="w-full h-screen">
