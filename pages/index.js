ef0dc0fb9ebcecffa5903980c4c54a56505f01d7
Jorge Carranza <ssgsj.carranza@gmail.com>
2022-01-03 20:20:11 -0600

    created components folder, working on login function first

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Login from '../components/Login'
 
 export default function Home() {
   return (
@@ -7,6 +8,7 @@ export default function Home() {
         <title>Metaverse</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Login />
     </div>
   )
 }
