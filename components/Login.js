f068051deebccec5671e4380f73e7655d1acd487
Jorge Carranza <ssgsj.carranza@gmail.com>
2022-01-03 20:28:34 -0600

    divided login component in different sections, pulled image tag from next js, adding config.js file to accept img source link

--- components/Login.js
+++ components/Login.js
@@ -1,11 +1,22 @@
-
+import Image from "next/image";
 
 function Login() {
     return (
         <div className="bg-black relative text-white">
-            <h1>Login</h1>        
+            <h1>Login</h1>
+            <div>
+                {/* logo */}
+                {/* login button */}
+            </div>
+
+            <div>
+                {/* background image */}
+                <Image 
+                    src='https://links.papareact.com/55n'
+                />
+            </div>        
         </div>
-    )
+    );
 }
 
 export default Login
