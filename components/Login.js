e1d62dfadd7cdfaf3c29d7a13903b4d38bc0d24f
Jorge Carranza <ssgsj.carranza@gmail.com>
2022-01-03 20:49:49 -0600

    white listed different links to change background and add logo

--- components/Login.js
+++ components/Login.js
@@ -2,12 +2,12 @@ import Image from "next/image";
 
 function Login() {
     return (
-        <div className="bg-black relative text-white">
+        <div className="bg-black relative">
             <h1>Login</h1>
-            <div className="flex flex-col absolute z-50 h-4/6 items-center justify-center w-full">
+            <div className="flex flex-col absolute z-50 h-4/6 items-center justify-center w-full space-y-4">
                 {/* logo */}
                 <Image 
-                    src='https://links.papareact.com/3pi'
+                    src='https://ml.globenewswire.com/Resource/Download/dc9ca8e0-ed7e-48d8-a73c-5b8278eb5a0a?size=2'
                     height={200}
                     width={200}
                 />
@@ -18,7 +18,7 @@ function Login() {
             <div className="w-full h-screen">
                 {/* background image */}
                 <Image 
-                    src='https://links.papareact.com/55n'
+                    src='https://venturebeat.com/wp-content/uploads/2021/09/virtual_reality.jpg?fit=3840%2C2160&strip=all'
                     layout='fill'
                     objectFit="cover"
                 />
