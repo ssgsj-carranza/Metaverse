7cd10bf55e24ee3c80dc48c6e8c25219734d0092
Jorge Carranza <ssgsj.carranza@gmail.com>
2022-01-03 20:53:34 -0600

    styled logo and login button

--- components/Login.js
+++ components/Login.js
@@ -6,13 +6,14 @@ function Login() {
             <h1>Login</h1>
             <div className="flex flex-col absolute z-50 h-4/6 items-center justify-center w-full space-y-4">
                 {/* logo */}
-                <Image 
+                <Image
+                    className="object-cover rounded-full" 
                     src='https://ml.globenewswire.com/Resource/Download/dc9ca8e0-ed7e-48d8-a73c-5b8278eb5a0a?size=2'
                     height={200}
                     width={200}
                 />
                 {/* login button */}
-                <button>Login into Metaverse</button>
+                <button className="bg-purple-400 rounded-lg p-5 font-bold animate-bounce">Login into Metaverse</button>
             </div>
 
             <div className="w-full h-screen">
