8442d35356e1fa4d8d8c37d7e3eb619888e3a9db
Jorge Carranza <ssgsj.carranza@gmail.com>
2022-01-10 20:37:11 -0600

    pulled in logo image to be displayed on the header and addeds tyling to it

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,21 @@
+import Image from "next/image";
+import { useMoralis } from "react-moralis";
 
 
 function Header() {
+    const {user} = useMoralis();
     return (
         <div>
-            <h1>Header here</h1>        
+            <div>
+                <div className="relative h-24 w-24 mx-auto hidden lg:inline-grid">
+                    <Image
+                        layout='fill'
+                        objectFit='cover'
+                        className="rounded-full" 
+                        src='https://ml.globenewswire.com/Resource/Download/dc9ca8e0-ed7e-48d8-a73c-5b8278eb5a0a?size=2' 
+                    />
+                </div>
+            </div>        
         </div>
     )
 }
