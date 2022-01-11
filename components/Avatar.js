8c1343f9a87889ec73213d85392b60e8d9256d7f
Jorge Carranza <ssgsj.carranza@gmail.com>
2022-01-10 20:42:09 -0600

    created avatar component, rendered it in header, adding logout on press functionality on avatar

---
+++ components/Avatar.js
@@ -0,0 +1,14 @@
+import { useMoralis } from "react-moralis";
+
+
+function Avatar({username, logoutOnPress}) {
+    const {user, logout} = useMoralis();
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Avatar
--- components/Header.js
+++ components/Header.js
@@ -1,12 +1,13 @@
 import Image from "next/image";
 import { useMoralis } from "react-moralis";
+import Avatar from "./Avatar";
 
 
 function Header() {
     const {user} = useMoralis();
     return (
         <div>
-            <div>
+            <div className="">
                 <div className="relative h-24 w-24 mx-auto hidden lg:inline-grid">
                     <Image
                         layout='fill'
@@ -14,6 +15,16 @@ function Header() {
                         className="rounded-full" 
                         src='https://ml.globenewswire.com/Resource/Download/dc9ca8e0-ed7e-48d8-a73c-5b8278eb5a0a?size=2' 
                     />
+                </div>
+                
+                <div>
+                    <div>
+                        <Avatar logoutOnPress />        
+                    </div>
+                    {/* welcome message */}
+                    {/* username */}
+
+                    {/* change username */}
                 </div>
             </div>        
         </div>
