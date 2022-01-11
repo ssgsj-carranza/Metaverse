23e236e88905358f122dead8d79cee403ef41cd2
Jorge Carranza <ssgsj.carranza@gmail.com>
2022-01-10 20:23:47 -0600

    begining work on header component

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Login from '../components/Login'
 import { useMoralis } from 'react-moralis';
+import Header from '../components/Header';
 
 export default function Home() {
   const {isAuthenticated, logout} = useMoralis();
@@ -14,9 +15,18 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <h1>Welcome to Metaverse</h1>
+      
+      <div>
+        <div className='max-w-screen-2xl mx-auto'>
+          {/* header */}
+          <Header />
+          {/* messages */}
+        </div>
+      </div>
+      
       <button onClick={logout}>Logout</button>
     </div>
-  )
+  );
 }
 
 
