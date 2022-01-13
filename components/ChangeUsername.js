bc13870491c98e4205061f35e6e47f31b9dffa5f
Jorge Carranza <ssgsj.carranza@gmail.com>
2022-01-12 20:22:58 -0600

    created change username function to prompt user to change username, and then updated, added conditional to prevent spamming of change user name button, added disabled button if the username is updating

--- components/ChangeUsername.js
+++ components/ChangeUsername.js
@@ -4,9 +4,22 @@ import { useMoralis } from "react-moralis";
 function ChangeUsername() {
     const {setUserData, isUserUpdating, userError, user} = useMoralis();
 
+    const setUsername = () => {
+        const username = prompt(`Enter your new Username (current: ${user.getUsername()})`);
+        if (!username) return;
+        //updates
+        setUserData({
+            username,
+        });
+    };
+
     return (
         <div className="text-sm absolute top-5 right-5">
-            <button className="hover:text-pink-700">Change your Username</button>
+            <button disabled={isUserUpdating} 
+                    className="hover:text-pink-700"
+                    onClick={setUsername}
+            >Change your Username
+            </button>
         </div>
     )
 }
