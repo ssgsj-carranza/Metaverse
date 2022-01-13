2dd5ec6b883f8b02e4c1bc55332c305146ca2ea2
Jorge Carranza <ssgsj.carranza@gmail.com>
2022-01-12 20:47:01 -0600

    divided messages component into sections required, created message to display when messages are up to displaying the current users name

--- components/Messages.js
+++ components/Messages.js
@@ -1,14 +1,26 @@
 import {ByMoralis, useMoralis, useMoralisQuery} from 'react-moralis';
 
 function Messages() {
-
+    const {user} = useMoralis();
 
     return (
         <div className="pb-56">
             <div className="my-5">
                 <ByMoralis variant='dark' style={{marginLeft:'auto', marginRight:'auto'}} />
             </div>
-            <h1>Mesages</h1>        
+
+            <div>
+                {/* messages render */}
+            </div>
+
+            <div>
+                {/* <SendMessage /> */}
+            </div>
+
+            <div>
+                <p>You're up to date {user.getUsername()}! 🎉</p>
+            </div>
+            
         </div>
     )
 }
