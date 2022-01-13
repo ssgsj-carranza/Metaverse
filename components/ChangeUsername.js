ae76cd4760cfcd7531312dc1c872fec712395048
Jorge Carranza <ssgsj.carranza@gmail.com>
2022-01-12 20:15:53 -0600

    added button to change user name with some styling, workin on logic for changing user name, pulled in elements from moralis that Ill require to change username and update it

--- components/ChangeUsername.js
+++ components/ChangeUsername.js
@@ -1,9 +1,12 @@
+import { useMoralis } from "react-moralis";
 
 
 function ChangeUsername() {
+    const {setUserData, isUserUpdating, userError, user} = useMoralis();
+
     return (
-        <div>
-            
+        <div className="text-sm absolute top-5 right-5">
+            <button className="hover:text-pink-700">Change your Username</button>
         </div>
     )
 }
