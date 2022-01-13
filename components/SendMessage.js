6be7af0fcfd4af1cab8b586241e6af52009f9b62
Jorge Carranza <ssgsj.carranza@gmail.com>
2022-01-12 20:57:05 -0600

    finished styling input form, working on logic part next

--- components/SendMessage.js
+++ components/SendMessage.js
@@ -4,8 +4,8 @@ function SendMessage() {
     const {user} = useMoralis();
 
     return (
-        <form>
-            <input  className='outline-none bg-transparent text-white placeholder-gray-500' 
+        <form className='flex w-11/12 fixed bottom-10 bg-black opacity-80 px-6 py-4 max-w-2xl shadow-xl rounded-full border border-blue-400'>
+            <input  className='flex-grow outline-none bg-transparent text-white placeholder-gray-500 pr-5' 
                     type='text' 
                     placeholder={`Enter a Message ${user.getUsername()}...`}        
             />
