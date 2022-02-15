d35f3350b8cb910d4d14d3b0092402c90d13cb62
Jorge Carranza <ssgsj.carranza@gmail.com>
2022-02-15 14:28:00 -0600

    added variable to determine if its our message being sent, working on styling

--- components/Message.js
+++ components/Message.js
@@ -4,9 +4,14 @@ import { useMoralis} from 'react-moralis';
 function Message({message}) {
     const {user} = useMoralis();
 
+//Identifies if its our message    
+    const isUserMessage = message.get('ethAddress') === user.get('ethAddress');
+
     return (
         <div>
-            <p>{message.get('message')}</p>
+            <div>
+                <p>{message.get('message')}</p>
+            </div>
         </div>
     )
 }
