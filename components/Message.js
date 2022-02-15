a9ea11096f2b9e4f911ce70484050d2b94524f32
Jorge Carranza <ssgsj.carranza@gmail.com>
2022-02-15 14:35:47 -0600

    added conditional to change bg of message box depending on if its the user or not, working on further styling

--- components/Message.js
+++ components/Message.js
@@ -1,4 +1,5 @@
 import { useMoralis} from 'react-moralis';
+import Avatar from './Avatar';
 
 
 function Message({message}) {
@@ -8,8 +9,14 @@ function Message({message}) {
     const isUserMessage = message.get('ethAddress') === user.get('ethAddress');
 
     return (
-        <div>
-            <div>
+        <div className={`flex items-end space-x-2 relative ${isUserMessage && 'justify-end'}`}>
+            <div className='relative h-8 w-8'>
+                <Avatar username={message.get('username')} />
+            </div>
+            
+            <div className={`flex space-x-4 p-3 rounded-lg ${isUserMessage 
+                ? 'rounded-br-none bg-pink-300' 
+                : 'rounded-bl-none bg-blue-400'}`}>
                 <p>{message.get('message')}</p>
             </div>
         </div>
