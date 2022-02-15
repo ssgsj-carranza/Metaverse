b364320b3500e0e4374ddc625700c96479dcb5e5
Jorge Carranza <ssgsj.carranza@gmail.com>
2022-02-15 12:55:28 -0600

    working on creating a send message function, however sending message throught the blockchain is expensive, function creates a messages table in the moralis database and returns and object, using the etherum address to track messages

--- components/SendMessage.js
+++ components/SendMessage.js
@@ -5,6 +5,23 @@ function SendMessage() {
     const {user, Moralis} = useMoralis();
     const [message, setMessage] = useState('');
 
+    const sendMessage = (e) => {
+        e.preventDefault();
+
+        if(!message) return;
+
+        //Creates a table in the data base for messages, prepping it
+        const Messages = Moralis.Object.extend('Messages');
+        //Gives us object to update or pushover to moralis
+        const messages = new Messages();
+
+        messages.save({
+            message: message,
+            username: user.getUsername(),
+            ethAdress: user.get('ethAddress')
+        })
+    };
+
     return (
         <form className='flex w-11/12 fixed bottom-10 bg-black opacity-80 px-6 py-4 max-w-2xl shadow-xl rounded-full border border-blue-400'>
             <input  className='flex-grow outline-none bg-transparent text-white placeholder-gray-500 pr-5' 
@@ -13,7 +30,7 @@ function SendMessage() {
                     value={message}
                     onChange={(e) => setMessage(e.target.value)}        
             />
-            <button className='font-bold text-pink-500'>Send</button>        
+            <button type='submit' onClick={sendMessage} className='font-bold text-pink-500'>Send</button>        
         </form>
     )
 }
